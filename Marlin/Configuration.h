2ac6f678b930bd3673e84bb1fc7730d70ffa2a06
heavylight-repo <tobbeerlandsson1@gmail.com>
2020-05-18 22:25:34 +0200

    Test 4

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -88,7 +88,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Test 3, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Test 4, default config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
